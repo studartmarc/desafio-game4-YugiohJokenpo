a7317162a5518b0fa8c85919466ed43fb3729b90
Marcelo Studart <studartmarc@gmail.com>
2024-02-19 16:19:35 -0300

    organização de pastas do projeto

---
+++ desafio-yugioh/src/scripts/engine.js
@@ -0,0 +1,101 @@
+const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById("score_points"),
+    },
+    cardSprites: {
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+    fieldCards: {
+        player: document.getElementById("player-field-card"),
+        computer: document.getElementById("computer-field-card"),
+    },
+    actions: {
+        button: document.getElementById("next-duel"),
+    },
+};
+
+const playerSides = {
+    player1: "player-cards",
+    computer: "computer-cards",
+};
+
+const cardData = [
+    {
+        id: 0,
+        name: "Blue Eyed White Dragon",
+        type: "Paper",
+        img: "./src/assets/icons/dragon.png",
+        WinOf: [1],
+        LoseOf: [2],
+    },
+    {
+        id: 1,
+        name: "Dark Magician",
+        type: "Rock",
+        img: "./src/assets/icons/magician.png",
+        WinOf: [2],
+        LoseOf: [0],
+    },
+    {
+        id: 2,
+        name: "Exodia",
+        type: "Scissors",
+        img: "./src/assets/icons/exodia.png",
+        WinOf: [0],
+        LoseOf: [1],
+    },
+];
+
+const players = {
+    player1: "player-cards",
+};
+
+async function getRandomCardId() {
+    const randomIndex = Math.floor(Math.random() * cardData.length);
+    return cardData[randomIndex].id;
+};
+
+async function createCardImage(IdCard, fieldSide) {
+    const cardImage = document.createElement("img");
+    cardImage.setAttribute("height", "100px");
+    cardImage.setAttribute("src", "./src/assets/icons/card-back.png");
+    cardImage.setAttribute("data-id", IdCard);
+    cardImage.classList.add("card");
+
+    if (fieldSide === playerSides.player1) {
+        cardImage.addEventListener("mouseover", () => {
+            drawSelectCard(IdCard);
+        });
+        cardImage.addEventListener("click", () => {
+            setCardsField(cardImage.getAttribute("data-id"));
+        });
+    };
+
+    return cardImage;
+};
+
+async function drawSelectCard(index) {
+    state.cardSprites.avatar.src = cardData[index].img;
+    state.cardSprites.name.innerText = cardData[index].name;
+    state.cardSprites.type.innerText = "Attribute : " + cardData[index].type;
+};
+
+async function drawCards(cardNumber, fieldSide) {
+    for (let i = 0; i < cardNumber; i++) {
+        const randomIdCard = await getRandomCardId();
+        const cardImage = await createCardImage(randomIdCard, fieldSide);
+
+        document.getElementById(fieldSide).appendChild(cardImage);
+    };
+};
+
+function init() {
+    drawCards(5, playerSides.player1);
+    drawCards(5, playerSides.computer);
+}
+
+init();
