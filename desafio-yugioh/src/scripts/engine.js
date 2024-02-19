8975612a2de7d81745198c5e20566744315ea043
Marcelo Studart <studartmarc@gmail.com>
2024-02-19 18:23:39 -0300

    implementação final da lógica da condição de vitória, interação dos audios e personalização dos cursores

--- desafio-yugioh/src/scripts/engine.js
+++ desafio-yugioh/src/scripts/engine.js
@@ -78,6 +78,80 @@ async function createCardImage(IdCard, fieldSide) {
     return cardImage;
 };
 
+async function setCardsField(cardId) {
+    await removeAllCardsImages();
+
+    let computerCardId = await getRandomCardId();
+
+    await showOrHideCardFieldDetails(true);
+
+    await hideCardDetails();
+
+    state.fieldCards.player.src = cardData[cardId].img;
+    state.fieldCards.computer.src = cardData[computerCardId].img;
+
+    let duelResult = await checkDuelResult(cardId, computerCardId);
+
+    await updateScore();
+    await drawButton(duelResult);
+};
+
+async function checkDuelResult(playerCardId, computerCardId) {
+    let duelResult = "DRAW";
+    let playerCard = cardData[playerCardId];
+
+    if (playerCard.WinOf.includes(computerCardId)) {
+        duelResult = "WIN";
+        await playAudio(duelResult);
+        state.score.playerScore++;
+    };
+
+    if (playerCard.LoseOf.includes(computerCardId)) {
+        duelResult = "LOSE";
+        await playAudio(duelResult);
+        state.score.computerScore++;
+    };
+
+    return duelResult;
+};
+
+async function showOrHideCardFieldDetails(value) {
+    if (value === true) {
+        state.fieldCards.player.style.display = "block";
+        state.fieldCards.computer.style.display = "block";
+    }
+
+    if (value === false) {
+        state.fieldCards.player.style.display = "none";
+        state.fieldCards.computer.style.display = "none";
+    };
+};
+
+async function hideCardDetails() {
+    state.cardSprites.avatar.src = "";
+    state.cardSprites.name.innerText = "";
+    state.cardSprites.type.innerText = "";
+};
+
+async function drawButton(text) {
+    state.actions.button.innerText = text;
+    state.actions.button.style.display = "block";
+};
+
+async function updateScore() {
+    state.score.scoreBox.innerText = `Win: ${state.score.playerScore} | Lose:${state.score.computerScore}`
+};
+
+async function removeAllCardsImages() {
+    let cards = document.querySelector("#computer-cards");
+    let imgElements = cards.querySelectorAll("img");
+    imgElements.forEach((img) => img.remove());
+
+    cards = document.querySelector("#player-cards");
+    imgElements = cards.querySelectorAll("img");
+    imgElements.forEach((img) => img.remove());
+}
+
 async function drawSelectCard(index) {
     state.cardSprites.avatar.src = cardData[index].img;
     state.cardSprites.name.innerText = cardData[index].name;
@@ -93,9 +167,30 @@ async function drawCards(cardNumber, fieldSide) {
     };
 };
 
+async function resetDuel() {
+    state.cardSprites.avatar.src = "";
+    state.actions.button.style.display = "none";
+
+    state.fieldCards.player.style.display = "none";
+    state.fieldCards.computer.style.display = "none";
+
+    init()
+};
+
+async function playAudio(status) {
+    const audio = new Audio(`./src/assets/audios/${status}.wav`);
+    audio.play();
+};
+
 function init() {
+    showOrHideCardFieldDetails(false);
+
     drawCards(5, playerSides.player1);
     drawCards(5, playerSides.computer);
-}
+
+    // comando para iniciar música
+    // const bgm = document.getElementById("bgm")
+    // bgm.play();
+};
 
 init();
